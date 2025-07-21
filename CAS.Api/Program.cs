a37cf26f9eb97e351e49e434f64cdba6ab3f64a8
kristan66 <kristan@hotmail.co.uk>
2025-07-21 11:09:23 +0100

    Initial structure - migrated essentials across from starter template, built basic api and enabled VS debugging.

--- CAS.Api/Program.cs
+++ CAS.Api/Program.cs
@@ -1,17 +1,31 @@
+using CAS.Api.Bootstrap;
 using CAS.Core;
+using FluentValidation;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddCustomTrustStore();
+builder.Services.AddHttpContextAccessor();
+builder.Host.UseSerilog(CdpLogging.Configuration);
+builder.Services.AddHttpClient("DefaultClient").AddHeaderPropagation();
+builder.Services.AddTransient<ProxyHttpMessageHandler>();
+builder.Services.AddHttpClient("proxy").ConfigurePrimaryHttpMessageHandler<ProxyHttpMessageHandler>();
+builder.Services.AddHeaderPropagation(options =>
+{
+    var traceHeader = builder.Configuration.GetValue<string>("TraceHeader");
+    if (!string.IsNullOrWhiteSpace(traceHeader))
+    {
+        options.Headers.Add(traceHeader);
+    }
+});
+builder.Services.AddHealthChecks();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddControllers();
 builder.Services.AddCasCore();
 
-
 var app = builder.Build();
-
-
-app.UseHttpsRedirection();
-
+//app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,34 @@
-# Base dotnet image
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
 WORKDIR /app
 EXPOSE 80
 EXPOSE 443
-
-# Add curl to template.
-# CDP PLATFORM HEALTHCHECK REQUIREMENT
+EXPOSE 8080
+EXPOSE 8081
 RUN apt update && \
     apt install curl -y && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# Build stage image
+
+
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
 WORKDIR /src
-
 COPY . .
-WORKDIR "/src"
-
-# unit test and code coverage
+RUN dotnet restore "CAS.Api/CAS.Api.csproj"
 RUN dotnet test CAS.Test
+RUN dotnet build "CAS.Api/CAS.Api.csproj" -c $BUILD_CONFIGURATION -o /app/build /p:UseAppHost=false
+
+
+
 
 FROM build AS publish
-RUN dotnet publish CAS.Api -c Release -o /app/publish /p:UseAppHost=false
-
-
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "CAS.Api/CAS.Api.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
 ENV ASPNETCORE_FORWARDEDHEADERS_ENABLED=true
 
-# Final production image
+
+
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
